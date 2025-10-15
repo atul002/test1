23b0991370b17c4c445e2931e9171b2cd5b62c75
Atul <002.atulsingh@gmail.com>
2025-10-15 21:20:48 +0530

    child added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Child],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
