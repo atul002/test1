decfbdcc9ee44288984c41cc97e5fb1fe373d325
Atul <002.atulsingh@gmail.com>
2025-10-15 21:42:20 +0530

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Child } from './child/child';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [    
+    {path: '', component: Home},
+    { path: 'child', component: Child }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Child],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
